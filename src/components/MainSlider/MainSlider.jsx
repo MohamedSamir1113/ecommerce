adfd3efa569407fb647dfc93742bf424fa6a6d65
MohamedSamir1113 <simo2000854@gmail.com>
2024-06-27 16:37:20 +0200

    set up

---
+++ src/components/MainSlider/MainSlider.jsx
@@ -0,0 +1,10 @@
+import styles from './MainSlider.module.css'
+function MainSlider() {
+    return (
+        <div>
+            MainSlider
+        </div>
+    )
+}
+
+export default MainSlider
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import CategorySlider from "../../components/CategorySlider/CategorySlider";
 import FeatuedProducts from "../../components/FeaturedProducts/FeatuedProducts";
+import MainSlider from "../../components/MainSlider/MainSlider";
 
 function Home() {
   return (
     <>
+      <MainSlider />
       <CategorySlider />
       <FeatuedProducts />
     </>
