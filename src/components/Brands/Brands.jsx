c4648802f7242b920a21c4aa13daec4d7d830a26
MohamedSamir1113 <simo2000854@gmail.com>
2024-07-07 15:43:38 +0300

    get Brands

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,4 +1,15 @@
+import axios from "axios"
+import { useQuery } from "react-query";
+
 function Brands() {
+    async function getBrands() {
+        const response = await axios.get("https://ecommerce.routemisr.com/api/v1/brands");
+       
+        return response.data
+    }
+
+    const{data,isLoading}=useQuery("getBrands",getBrands);
+    console.log(data?.data);
     return (
         <div>
             
