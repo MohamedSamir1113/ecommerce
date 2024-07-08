c78605d4c845715876a9c6174d7ce492c64bf5cf
MohamedSamir1113 <simo2000854@gmail.com>
2024-07-08 09:54:14 +0300

    added basic footer

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from "react-router"
-import Navbar from "../../components/Navbar/Navbar"
+import { Outlet } from "react-router";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 function Layout() {
-    return (
-        <div>
-            <Navbar/>
-            <Outlet>
-                
-            </Outlet>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <Outlet></Outlet>
+      <Footer/>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
